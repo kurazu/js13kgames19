83cedaad2fd2e303b87e2c732bde2324679365a9
Tomasz Maćkowiak <kurazu@kurazu.net>
2019-09-10 08:02:32 +0200

    utils tests

--- tests/utils.spec.ts
+++ tests/utils.spec.ts
@@ -1,5 +1,21 @@
 import { uniformRandom, range } from '../src/utils';
 
+describe('range', () => {
+    const testData: [number, number[]][] = [
+        [0, []],
+        [1, [0]],
+        [2, [0, 1]],
+        [3, [0, 1, 2]],
+        [4, [0, 1, 2, 3]],
+        [5, [0, 1, 2, 3, 4]]
+    ];
+    for (const [n, expected] of testData) {
+        test(`generates a sequence for n=${n}`, () => {
+            expect(range(n)).toEqual(expected);
+        });
+    }
+});
+
 describe('uniformRandom', () => {
     test('generates range <-1, 1>', () => {
         const results = range(1000).map(uniformRandom);
